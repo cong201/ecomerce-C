17b7e386c69a73b8b957bfb425dd132912604d16
Le Van Cong <conglevan21072001@innofly-software.com>
2025-02-06 13:16:48 +0700

    add api review

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,7 @@ import cartReducer from "./feature/cart/cartSlice";
 import authApi from "./feature/auth/authApi";
 import authReducer from "./feature/auth/authSlice";
 import productsApi from "./feature/products/productsApi";
+import reviewApi from "./feature/reviews/reviewsApi";
 
 export const store = configureStore({
   reducer: {
@@ -10,7 +11,12 @@ export const store = configureStore({
     [authApi.reducerPath]: authApi.reducer,
     auth: authReducer,
     [productsApi.reducerPath]: productsApi.reducer,
+    [reviewApi.reducerPath]: reviewApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware, productsApi.middleware),
+    getDefaultMiddleware().concat(
+      authApi.middleware,
+      productsApi.middleware,
+      reviewApi.middleware
+    ),
 });
